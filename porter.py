3fad5de45a031671f49c5657db2617429de2b596
AndreiShalupaey <39306897+AndreiShalupaey@users.noreply.github.com>
2020-06-25 01:45:08 +0300

    Update porter.py

--- porter.py
+++ porter.py
@@ -1,7 +1,11 @@
-from termcolor import colored
-
 import socket
 import sys
+import os
+
+try:
+    from termcolor import colored
+except ModuleNotFoundError:
+    os.system("pip install termcolor")
 
 def help():
     print(colored("--------------------------------------", "red"))
@@ -14,31 +18,60 @@ def help():
     print()
     print("Usage: python porter.py [Target IP] [Options]")
     print()
-    print("-h, --help  --> show this window")
-    print("-a, --all   --> show close and open ports")
-    print("-o, --open  --> show only open ports")
-    print("-c, --close --> show only closed ports")
+    print("-h, --help        --> show this window")
+    print("-a, --all         --> show close and open ports")
+    print("-o, --open        --> show only open ports")
+    print("-c, --close       --> show only closed ports")
+    print("-p, --port <port> --> scan only one port")
     print()
     print("Example: python porter.py 8.8.8.8 --all")
     
-def main(ip, mode):
+def main(ip, mode, port = None):
     scanner = socket.socket()
 
-    for i in range(65536):
+    if (port == None):
+        for i in range(65536):
+            try:
+                scanner.connect((ip, i))
+            except OSError:
+                if (((mode == "-a") or (mode == "-c")) or ((mode == "--all") or (mode == "--close"))):
+                    print(colored("[!]", "red") + " PORT " + str(i) + " CLOSED")
+            else:
+                if (((mode == "-a") or (mode == "-o")) or ((mode == "--all") or (mode == "--open"))):
+                    print(colored("[!]", "yellow") + " PORT " + str(i) + " OPEN")
+    else:
         try:
-            scanner.connect((ip, i))
+            scanner.connect((ip, port))
         except OSError:
             if (((mode == "-a") or (mode == "-c")) or ((mode == "--all") or (mode == "--close"))):
-                print(colored("[!]", "red") + " PORT " + str(i) + " CLOSED")
+                print(colored("[!]", "red") + " PORT " + str(port) + " CLOSED")
         else:
             if (((mode == "-a") or (mode == "-o")) or ((mode == "--all") or (mode == "--open"))):
-                print(colored("[!]", "yellow") + " PORT " + str(i) + " OPEN")
+                print(colored("[!]", "yellow") + " PORT " + str(port) + " OPEN")
     
 if __name__=="__main__":
     try:
-        if ((sys.argv[1] == "-h") or (sys.argv[1] == "--help")):
-            help()
+        if (len(sys.argv) <= 3):
+            if ((sys.argv[1] == "-h") or (sys.argv[1] == "--help")):
+                help()
+            else:
+                main(sys.argv[1], sys.argv[2])
         else:
-            main(sys.argv[1], sys.argv[2])
+            if ((sys.argv[2] == "-p") or (sys.argv[2] == "--port")):
+                try:
+                    port = int(sys.argv[3])
+                except ValueError:
+                    print("Invalid port")
+                    exit()
+                main(sys.argv[1], sys.argv[4], port)
+            elif ((sys.argv[3] == "-p") or (sys.argv[3] == "--port")):
+                try:
+                    port = int(sys.argv[4])
+                except ValueError:
+                    print("Invalid port")
+                    exit()
+                main(sys.argv[1], sys.argv[2], port)
+            else:
+                help()
     except IndexError:
         help()
